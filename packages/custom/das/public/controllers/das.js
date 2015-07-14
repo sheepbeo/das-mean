9a25fb7853b25c57b753f4396b0618afb5348859
sheepbeo <lequoccuong91@yahoo.com>
2015-07-14 22:49:09 +0300

    - modify header style

--- packages/custom/das/public/controllers/das.js
+++ packages/custom/das/public/controllers/das.js
@@ -20,40 +20,97 @@ angular.module('mean.das', ['chart.js']).controller('DasController', ['$scope', 
 		[0, 0, 0, 0, 0, 0, 0]
 	];
 
-    $scope.getAllSpeedups = function() {
-		Speedup.scope = $scope;
-		Speedup.getAll()
-			.success(function(data) {
-				var speedups = data;
+	Chart.defaults.global.colours[0] = "#F27952";
 
-				var i = 0;
+	var allSpeedups = [];
+	var timeMarks = [0, 5, 10, 30, 60, 120, 240, 480, 1440];
 
-				for (i=0; i<speedups.length; i++) {
-					var speedup = speedups[i];
+	function getHorizontalValues(type) {
+    	var values = [];
 
-					console.log(speedup.timeLeft);
+    	if (type == "timeLeft") {
+    		for (var i=0; i<timeMarks.length; i++) {
+    			if (i != timeMarks.length - 1) {
+    				values.push(timeMarks[i] + "-" + timeMarks[i+1]);
+    			} else {
+    				values.push(timeMarks[i] + "+");
+    			}
+    		}
+    	}
+
+    	return values;
+    }
+
+    function getVerticalValues(typeHoz, typeVer) {
+    	var values = [];
+
+    	for (var i=0; i<getHorizontalValues(typeHoz).length; i++) {
+    		values.push(0);
+    	}
+
+    	if (typeVer == "count") {
+    		if (typeHoz == "timeLeft") {
+    			for (var i=0; i<allSpeedups.length; i++) {
+					var speedup = allSpeedups[i];
+
 					var times = speedup.timeLeft.split(':');
 					var totalMinutes = parseInt(times[0]) * 60 + parseInt(times[1]);
 
-					var index = Math.floor(totalMinutes / 5);
-					console.log(times);
-					console.log(index);
-					console.log(parseInt(times[1]));
-					
-					index = Math.min(index, $scope.data[0].length-1);
-					console.log(index);
-
-					$scope.data[0][index] += 1;
+					var index = getIndexFromRange(totalMinutes, timeMarks);
+					values[index]++;
 				}
+    		}
+    	}
 
+    	return values;
+    }
+
+    function getIndexFromRange(number, rangeArray) {
+    	if (rangeArray.length > 0) {
+    		for (var i=0; i<rangeArray.length-2; i++) {
+    			if (number < rangeArray[i+1]) {
+    				return i;
+    			}
+    		}
+
+    		return rangeArray.length-1;
+    	}
+
+    	return false;
+    }
+
+    $scope.getAllSpeedups = function(callBack) {
+		Speedup.scope = $scope;
+		Speedup.getAll()
+			.success(function(data) {
+				allSpeedups = data;
+				callBack(true);
 			})
 			.error(function(error) {
-
+				callBack(false);
 			});
     };
-    $scope.getAllSpeedups();
+    
+
+    $scope.optionVertical = "count";
+    $scope.optionHorizontal = "timeLeft";
+
+    $scope.updateGraph = function() {
+		$scope.labels = getHorizontalValues($scope.optionHorizontal);
+		$scope.data[0] = getVerticalValues($scope.optionHorizontal, $scope.optionVertical);
+    }
+
+    
+
+    
 
 
+    // initialization
+    $scope.getAllSpeedups(function(result) {
+    	if (result) {
+    		$scope.updateGraph();
+    	}
+    });
   }
 ]);
 
